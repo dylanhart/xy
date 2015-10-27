b5d5318ad2f61f3427eb6b2fd134f817870b1d85
dylanhart <dylan96hart@gmail.com>
2015-10-26 19:31:30 -0700

    move gridstate out of dom and into js

--- game.js
+++ game.js
@@ -1,15 +1,43 @@
 
 var xy = {};
 
+// state "enum"
+xy.TileState = {
+	EMPTY: {},
+	X: {
+		cssclass: "x"
+	},
+	Y: {
+		cssclass: "y"
+	}
+}
+
+// get next state
+xy.TileState.next = function(state) {
+	if (state === this.EMPTY)
+		return this.X;
+	if (state === this.X)
+		return this.Y;
+
+	// default to empty
+	return this.EMPTY;
+}
+
 xy.Grid = function(parent, size) {
+	this.gridState = [];
+
 	this.grid = document.createElement("div");
 	this.grid.className = "grid grid" + size;
 
 	for (var r = 0; r < size; r++) {
+		var states = [];
+
 		var row = document.createElement("div");
 		row.className = "grid-row";
 
 		for (var c = 0; c < size; c++) {
+			states.push(xy.TileState.EMPTY);
+
 			var tile = document.createElement("div");
 			tile.className = "tile";
 
@@ -23,6 +51,8 @@ xy.Grid = function(parent, size) {
 		}
 
 		this.grid.appendChild(row);
+
+		this.gridState.push(states);
 	}
 
 	parent.appendChild(this.grid);
@@ -33,12 +63,12 @@ xy.Grid.prototype.tileClicked = function(event, r, c) {
 
 	var tile = event.target;
 
-	if (tile.classList.contains("x")) {
-		tile.className = "tile y";
-	} else if (tile.classList.contains("y")) {
-		tile.className = "tile";
+	var state = this.gridState[r][c] = xy.TileState.next(this.gridState[r][c]);
+
+	if (state.cssclass) {
+		tile.className = "tile " + state.cssclass;
 	} else {
-		tile.className = "tile x";
+		tile.className = "tile";
 	}
 }
 
